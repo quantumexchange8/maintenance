d3d5664d427c6d5c8b9b37d5ce74ede8051c9193
tonglim <tonglim88@gmail.com>
2024-12-01 15:04:54 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,11 +9,21 @@
 </head>
 <body class="bg-dark">
     <div class="col-lg-12">
-        <div class="container text-center">
+        <div class="container text-left">
             <img src="{{ asset('web_maintenance.gif') }}" class="img-fluid" style="padding-top:30px; ">
-            <h1 class="text-light" style="padding-top:20px;">Under Construction - Will be back</h1>
-            <p class="text-light">Our website is currently undergoing scheduled maintenance. <br> Should be back shortly. Thank you for your patience.</p>
-            <p class="text-light">我们的网站目前正在进行定期维护。 <br>应该很快就会回来。 感谢您的耐心等待。</p>
+            <h1 class="text-light" style="padding-top:20px;">升級中</h1>
+            <p class="text-light">親愛的會員們，<br><br>
+我們非常感謝會員一直以來支持環球量子資本 (QUANTUM CAPITAL GLOBAL)。我們的個人後臺系統及全新介面將於 今晚12點 (12月1日) 升級完成，<br>新系統域名將更改為：<a href="https://login.qcgexchange.com">https://login.qcgexchange.com</a>。<br><br>
+
+此次升級旨在提供更好的使用體驗，提高交易質量，並引入更多實用的新功能，幫助會員更高效地管理交易賬戶和查看帳目明細。此外，請注意以下事項：<br><br>
+
+    現金錢包功能停止： 在升級完成後，現金錢包功能將全面停止，會員需在截止日期 (11月30日之前) 將現金錢包內的餘額轉移至交易賬戶或提取資金。<br><br>
+    自動轉移餘額： 若截止時間後仍有餘額在現金錢包中，系統將自動為會員開設新交易賬戶，並將餘額轉移至新賬戶。<br>
+
+如有任何疑問，請聯繫我們的客戶服務郵箱：<a href="mailto:support@qcgbrokertw.com">support@qcgbrokertw.com</a>。<br><br>
+
+感謝您對 QCG 的支持與信任！</p>
+            
         </div>
         </div>
     </div>
