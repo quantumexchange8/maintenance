03a317c19e686e00425c945b44f1cb3f5f06e1ff
tonglim <tonglim88@gmail.com>
2024-12-01 15:15:28 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,24 +9,12 @@
 </head>
 <body class="bg-dark">
     <div class="col-lg-12">
-        <div class="container text-left">
+        <div class="container text-center">
             <img src="{{ asset('web_maintenance.gif') }}" class="img-fluid" style="padding-top:30px; ">
-            <h1 class="text-light" style="padding-top:20px;">升級中</h1>
-            <p class="text-light">親愛的會員們，<br><br>
-我們非常感謝會員一直以來支持環球量子資本 (QUANTUM CAPITAL GLOBAL)。我們的個人後臺系統及全新介面將於 今晚12點 (12月1日) 升級完成，<br>
-新域名將更改為：<a href="https://home.qcgexchange.com">https://home.qcgexchange.com</a><br>
-新系統域名將更改為：<a href="https://login.qcgexchange.com">https://login.qcgexchange.com</a><br><br>
-
-此次升級旨在提供更好的使用體驗，提高交易質量，並引入更多實用的新功能，幫助會員更高效地管理交易賬戶和查看帳目明細。此外，請注意以下事項：<br><br>
-
-    現金錢包功能停止： 在升級完成後，現金錢包功能將全面停止，會員需在截止日期 (11月30日之前) 將現金錢包內的餘額轉移至交易賬戶或提取資金。<br><br>
-    自動轉移餘額： 若截止時間後仍有餘額在現金錢包中，系統將自動為會員開設新交易賬戶，並將餘額轉移至新賬戶。<br>
-
-如有任何疑問，請聯繫我們的客戶服務郵箱：<a href="mailto:support@qcgbrokertw.com">support@qcgbrokertw.com</a>。<br><br>
-
-感謝您對 QCG 的支持與信任！</p>
-            
-        </div>
+            <h1 class="text-light" style="padding-top:20px;">Under Construction - Will be back</h1>
+            <p class="text-light">Our website is currently undergoing scheduled maintenance. <br> Should be back shortly. Thank you for your patience. <br> The new interface will be completed upgraded on 12:00 tonight (December 1st 2024)</p>
+            <p class="text-light">我們的網站目前正在進行定期維護。 <br>應該很快就會回來。 感謝您的耐心等待。 <br> 新介面將於 今晚12點 (12月1日) 升級完成</p>
+                    </div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
